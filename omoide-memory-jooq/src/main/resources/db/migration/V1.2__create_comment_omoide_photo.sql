b159016fccd78b280d15b9592646744570850a71
kadtewsd <26476963+kadtewsd@users.noreply.github.com>
2026-02-18 15:47:11 +0900

    外部キー制約は id に。名前がかぶってもきえるが、念の為 id で拡張できる余地を残す

--- omoide-memory-jooq/src/main/resources/db/migration/V1.2__create_comment_omoide_photo.sql
+++ omoide-memory-jooq/src/main/resources/db/migration/V1.2__create_comment_omoide_photo.sql
@@ -1,20 +1,20 @@
 -- Create table for storing comments on files
 CREATE TABLE comment_omoide_photo (
     id              BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
-    file_name       VARCHAR(255)    NOT NULL,
+    photo_id       VARCHAR(255)    NOT NULL,
     comment_seq             INT    NOT NULL DEFAULT 1,
     commenter       VARCHAR(255),
     comment_body    TEXT,
     created_at      TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by      VARCHAR(255),
     CONSTRAINT pk_comment_omoide_photo PRIMARY KEY (id),
-    CONSTRAINT fk_comment_omoide_photo_photo FOREIGN KEY (file_name) REFERENCES synced_omoide_photo (file_name) ON DELETE CASCADE,
-    CONSTRAINT uq_comment_omoide_photo_seq UNIQUE (file_name, comment_seq)
+    CONSTRAINT fk_comment_omoide_photo_photo FOREIGN KEY (photo_id) REFERENCES synced_omoide_photo (id) ON DELETE CASCADE,
+    CONSTRAINT uq_comment_omoide_photo_seq UNIQUE (photo_id, comment_seq)
 );
 
 COMMENT ON TABLE comment_omoide_photo IS '写真に対するコメント';
 COMMENT ON COLUMN comment_omoide_photo.id IS 'コメントID（サロゲートキー）';
-COMMENT ON COLUMN comment_omoide_photo.file_name IS '対象ファイル名（外部キー）';
+COMMENT ON COLUMN comment_omoide_photo.photo_id IS '対象ファイルのID（外部キー）';
 COMMENT ON COLUMN comment_omoide_photo.comment_seq IS '写真ごとのコメント連番';
 COMMENT ON COLUMN comment_omoide_photo.commenter IS '発言者名';
 COMMENT ON COLUMN comment_omoide_photo.comment_body IS 'コメント本文';
