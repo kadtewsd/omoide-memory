c54cb93e072de45411957778d2264b5e173a5334
kadtewsd <26476963+kadtewsd@users.noreply.github.com>
2026-02-19 12:59:02 +0900

    コメンターの顔写真をいれる (#20)

--- omoide-memory-jooq/src/main/resources/db/migration/V1.1__create_commenter.sql
+++ omoide-memory-jooq/src/main/resources/db/migration/V1.1__create_commenter.sql
@@ -2,6 +2,7 @@ CREATE TABLE omoide_memory.commenter (
     id          BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
     family_id   VARCHAR(255)    NOT NULL,
     name        VARCHAR(255)    NOT NULL,
+    icon   TEXT    NOT NULL,
     created_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by  VARCHAR(255),
     CONSTRAINT pk_commenter PRIMARY KEY (id),
@@ -12,5 +13,6 @@ COMMENT ON TABLE  omoide_memory.commenter IS 'コメント投稿者';
 COMMENT ON COLUMN omoide_memory.commenter.id IS 'コメント投稿者ID（サロゲートキー）';
 COMMENT ON COLUMN omoide_memory.commenter.family_id IS '家族ID';
 COMMENT ON COLUMN omoide_memory.commenter.name IS 'コメント投稿者名';
+COMMENT ON COLUMN omoide_memory.commenter.icon IS 'コメント投稿者のアイコン。Base64エンコード済み文字列で格納される。Bytea だと永続化時にバイナリにしないといけないが、投入用ツールがないため';
 COMMENT ON COLUMN omoide_memory.commenter.created_at IS 'レコード作成日時（タイムゾーン付き）';
 COMMENT ON COLUMN omoide_memory.commenter.created_by IS 'レコード作成者';
