0a3fb85c51944535c289bbb49a98a9f8a031f16a
kadtewsd <26476963+kadtewsd@users.noreply.github.com>
2026-02-26 17:01:15 +0900

    アップロード時は foreground で表示させてアップロード時の問題が起きにくくする (#69)

--- omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/worker/AutoGDriveUploadWorker.kt
+++ omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/worker/AutoGDriveUploadWorker.kt
@@ -10,6 +10,7 @@ import com.kasakaid.omoidememory.data.OmoideMemory
 import com.kasakaid.omoidememory.data.OmoideMemoryDao
 import com.kasakaid.omoidememory.data.OmoideMemoryRepository
 import com.kasakaid.omoidememory.data.OmoideUploadPrefsRepository
+import com.kasakaid.omoidememory.worker.WorkerHelper.createForegroundInfo
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
@@ -21,39 +22,45 @@ import kotlinx.coroutines.withContext
  * 自動アップロード有効時のみアップロードを試行するクラス
  */
 @HiltWorker
-class AutoGDriveUploadWorker @AssistedInject constructor(
-    @Assisted appContext: Context,
-    @Assisted workerParams: WorkerParameters,
-    private val omoideUploadPrefsRepository: OmoideUploadPrefsRepository,
-    private val gdriveUploader: GdriveUploader,
-    private val omoideMemoryRepository: OmoideMemoryRepository,
-) : CoroutineWorker(appContext, workerParams) {
-
-    companion object {
-        const val TAG = "AutoUploadWorker"
-    }
-
-    override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
-        try {
-            Log.d(TAG, "自動アップロード開始")
-            // 1. Check Auto-Upload Setting
-            if (!omoideUploadPrefsRepository.isAutoUploadEnabled()) {
-                Log.d(TAG, "Auto-Upload が無効。写真のアップロードはスキップ")
-                return@withContext Result.success()
-            }
-
-            val uploadResult = mutableListOf<Result>()
-            var current = 0
-            omoideMemoryRepository.getActualPendingFiles().collect { currentList: OmoideMemory ->
-                // currentList には、その時点で「見つかっている分（20, 40, 60...）」が流れてくる
-                Log.d(TAG, "${++current}件目を開始")
-                gdriveUploader.upload(sourceWorker = WorkManagerTag.Auto, pendingFile = currentList)
-            }
-            if (uploadResult.distinct().size == 1) Result.success()
-            else Result.failure()
-        } catch (e: Exception) {
-            Log.e(TAG, "例外が発生", e)
-            return@withContext Result.retry()
+class AutoGDriveUploadWorker
+    @AssistedInject
+    constructor(
+        @Assisted private val appContext: Context,
+        @Assisted workerParams: WorkerParameters,
+        private val omoideUploadPrefsRepository: OmoideUploadPrefsRepository,
+        private val gdriveUploader: GdriveUploader,
+        private val omoideMemoryRepository: OmoideMemoryRepository,
+    ) : CoroutineWorker(appContext, workerParams) {
+        companion object {
+            const val TAG = "AutoUploadWorker"
         }
+
+        override suspend fun doWork(): Result =
+            withContext(Dispatchers.IO) {
+                setForeground(appContext.createForegroundInfo("AutoUpload"))
+                try {
+                    Log.d(TAG, "自動アップロード開始")
+                    // 1. Check Auto-Upload Setting
+                    if (!omoideUploadPrefsRepository.isAutoUploadEnabled()) {
+                        Log.d(TAG, "Auto-Upload が無効。写真のアップロードはスキップ")
+                        return@withContext Result.success()
+                    }
+
+                    val uploadResult = mutableListOf<Result>()
+                    var current = 0
+                    omoideMemoryRepository.getActualPendingFiles().collect { currentList: OmoideMemory ->
+                        // currentList には、その時点で「見つかっている分（20, 40, 60...）」が流れてくる
+                        Log.d(TAG, "${++current}件目を開始")
+                        gdriveUploader.upload(sourceWorker = WorkManagerTag.Auto, pendingFile = currentList)
+                    }
+                    if (uploadResult.distinct().size == 1) {
+                        Result.success()
+                    } else {
+                        Result.failure()
+                    }
+                } catch (e: Exception) {
+                    Log.e(TAG, "例外が発生", e)
+                    return@withContext Result.retry()
+                }
+            }
     }
-}
