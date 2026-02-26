36524a672de7bf6075af5dae5b66192ed78dafd3
kadtewsd <26476963+kadtewsd@users.noreply.github.com>
2026-02-26 16:43:08 +0900

    ssid は状態の変化でしか取得できない。今のタイミングで取得できなかったため、Wifi に接続していて、アップロードの ssid がセットされていたら OK とした

--- omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/extension/WorkManagerExtension.kt
+++ omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/extension/WorkManagerExtension.kt
@@ -1,7 +1,9 @@
 package com.kasakaid.omoidememory.extension
 
 import android.util.Log
+import androidx.work.Constraints
 import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
@@ -24,20 +26,30 @@ object WorkManagerExtension {
         hashes: Array<String>,
         totalCount: Int,
     ) {
-        val workData = workDataOf(
-            "TARGET_HASHES" to hashes,
-            "TOTAL_COUNT" to totalCount,
-        )
-        val uploadRequest = OneTimeWorkRequestBuilder<GdriveUploadWorker>()
-            .setInputData(workData)
-            .addTag(GdriveUploadWorker.TAG)
-            .build()
+        val workData =
+            workDataOf(
+                "TARGET_HASHES" to hashes,
+                "TOTAL_COUNT" to totalCount,
+            )
+        val constraints =
+            Constraints
+                .Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .build()
+
+        val uploadRequest =
+            OneTimeWorkRequestBuilder<GdriveUploadWorker>()
+                .setInputData(workData)
+                .addTag(GdriveUploadWorker.TAG)
+                .setConstraints(constraints)
+                .build()
         val tag = "FileSelectionRoute"
         Log.d(tag, "選択されたhash ${hashes.size}件")
 
         // enqueueUniqueWork + REPLACE は 「名前（Unique Name）」を指定することで、ひとつの管理枠を作ります。
         // 唯一性の保証: 同じ名前のジョブがすでにキューにある場合、WorkManager が介入します。
-        //REPLACE の魔法: 新しいリクエストが来たら、**古い方を即座にキャンセル（中断）**して、新しい方を最初から実行します。
+        // REPLACE の魔法: 新しいリクエストが来たら、**古い方を即座にキャンセル（中断）**して、新しい方を最初から実行します。
         enqueueUniqueWork(
             "manual_upload",
             ExistingWorkPolicy.REPLACE, // これで「都度上書き」される
@@ -49,52 +61,45 @@ object WorkManagerExtension {
      * アップロード状態を監視します。
      * 現在の進捗とほぼ同じですが、念の為 Worker のステートで確認
      */
-    fun WorkManager.observeUploadingStateByManualTag(
-        viewModelScope: CoroutineScope,
-    ): StateFlow<Boolean> {
-        return observeUploadingState(
+    fun WorkManager.observeUploadingStateByManualTag(viewModelScope: CoroutineScope): StateFlow<Boolean> =
+        observeUploadingState(
             viewModelScope = viewModelScope,
             workManagerTag = WorkManagerTag.Manual,
         )
-    }
 
     private fun WorkManager.observeUploadingState(
         viewModelScope: CoroutineScope,
         workManagerTag: WorkManagerTag,
-    ): StateFlow<Boolean> {
-        return getWorkInfosByTagFlow(workManagerTag.value)
+    ): StateFlow<Boolean> =
+        getWorkInfosByTagFlow(workManagerTag.value)
             .map { infos ->
                 infos.any { it.state == WorkInfo.State.RUNNING || it.state == WorkInfo.State.ENQUEUED }
-            }
-            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), false)
-    }
+            }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), false)
 
-    fun WorkManager.observeProgressByManual(
-        viewModelScope: CoroutineScope
-    ): StateFlow<Pair<Int, Int>?> {
-        return observeProgress(
+    fun WorkManager.observeProgressByManual(viewModelScope: CoroutineScope): StateFlow<Pair<Int, Int>?> =
+        observeProgress(
             viewModelScope = viewModelScope,
             workManagerTag = WorkManagerTag.Manual,
         )
-    }
+
     /**
      * 現在の進捗を確認します。
      */
     private fun WorkManager.observeProgress(
         viewModelScope: CoroutineScope,
         workManagerTag: WorkManagerTag,
-    ): StateFlow<Pair<Int, Int>?> {
-        return getWorkInfosByTagFlow(workManagerTag.value).map { workInfos ->
-            Log.d("アップロード監視", "${workInfos.size}件のワークフロー")
-            val runningWork = workInfos.find { it.state == WorkInfo.State.RUNNING }
-            val progress = runningWork?.progress
-            if (progress != null) {
-                val current = progress.getInt("PROGRESS_CURRENT", 0)
-                val total = progress.getInt("PROGRESS_TOTAL", 0)
-                current to total
-            } else {
-                null
-            }
-        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), null)
-    }
+    ): StateFlow<Pair<Int, Int>?> =
+        getWorkInfosByTagFlow(workManagerTag.value)
+            .map { workInfos ->
+                Log.d("アップロード監視", "${workInfos.size}件のワークフロー")
+                val runningWork = workInfos.find { it.state == WorkInfo.State.RUNNING }
+                val progress = runningWork?.progress
+                if (progress != null) {
+                    val current = progress.getInt("PROGRESS_CURRENT", 0)
+                    val total = progress.getInt("PROGRESS_TOTAL", 0)
+                    current to total
+                } else {
+                    null
+                }
+            }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), null)
 }
--- omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/ui/MainViewModel.kt
+++ omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/ui/MainViewModel.kt
@@ -20,52 +20,58 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    application: Application,
-    private val omoideUploadPrefsRepository: OmoideUploadPrefsRepository,
-) : ViewModel() {
+class MainViewModel
+    @Inject
+    constructor(
+        application: Application,
+        private val omoideUploadPrefsRepository: OmoideUploadPrefsRepository,
+    ) : ViewModel() {
+        private val _isAutoUploadEnabled =
+            MutableStateFlow(omoideUploadPrefsRepository.isAutoUploadEnabled())
+        val isAutoUploadEnabled: StateFlow<Boolean> = _isAutoUploadEnabled.asStateFlow()
 
-    private val _isAutoUploadEnabled =
-        MutableStateFlow(omoideUploadPrefsRepository.isAutoUploadEnabled())
-    val isAutoUploadEnabled: StateFlow<Boolean> = _isAutoUploadEnabled.asStateFlow()
+        private val workManager = WorkManager.getInstance(application)
+        val isUploading: StateFlow<Boolean> =
+            workManager.observeUploadingStateByManualTag(
+                viewModelScope = viewModelScope,
+            )
+        val progress: StateFlow<Pair<Int, Int>?> =
+            workManager.observeProgressByManual(
+                viewModelScope = viewModelScope,
+            )
 
-    private val workManager = WorkManager.getInstance(application)
-    val isUploading: StateFlow<Boolean> = workManager.observeUploadingStateByManualTag(
-        viewModelScope = viewModelScope,
-    )
-    val progress: StateFlow<Pair<Int, Int>?> = workManager.observeProgressByManual(
-        viewModelScope = viewModelScope,
-    )
+        fun toggleAutoUpload(enabled: Boolean) {
+            omoideUploadPrefsRepository.setAutoUploadEnabled(enabled)
+            _isAutoUploadEnabled.value = enabled
 
-    fun toggleAutoUpload(enabled: Boolean) {
-        omoideUploadPrefsRepository.setAutoUploadEnabled(enabled)
-        _isAutoUploadEnabled.value = enabled
+            if (enabled) {
+                enqueuePeriodicWork()
+            } else {
+                workManager.cancelUniqueWork("AutoUploadWork")
+            }
+        }
 
-        if (enabled) {
-            enqueuePeriodicWork()
-        } else {
-            workManager.cancelUniqueWork("AutoUploadWork")
+        /**
+         * 自動アップロードをキューイングするメソッド
+         */
+        private fun enqueuePeriodicWork() {
+            // wifi の時だけワーカーを実行してね、という印
+            val constraints =
+                Constraints
+                    .Builder()
+                    .setRequiredNetworkType(NetworkType.UNMETERED)
+                    .setRequiresBatteryNotLow(true)
+                    .build()
+
+            val uploadWorkRequest =
+                PeriodicWorkRequestBuilder<AutoGDriveUploadWorker>(1, TimeUnit.HOURS)
+                    .setConstraints(constraints)
+                    .build()
+
+            workManager.enqueueUniquePeriodicWork(
+                "AutoUploadWork",
+                ExistingPeriodicWorkPolicy.KEEP,
+                uploadWorkRequest,
+            )
         }
     }
-
-    /**
-     * 自動アップロードをキューイングするメソッド
-     */
-    private fun enqueuePeriodicWork() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.UNMETERED)
-            .setRequiresBatteryNotLow(true)
-            .build()
-
-        val uploadWorkRequest =
-            PeriodicWorkRequestBuilder<AutoGDriveUploadWorker>(1, TimeUnit.HOURS)
-                .setConstraints(constraints)
-                .build()
-
-        workManager.enqueueUniquePeriodicWork(
-            "AutoUploadWork",
-            ExistingPeriodicWorkPolicy.KEEP,
-            uploadWorkRequest
-        )
-    }
-}
