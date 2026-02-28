f2070a8d52526aa7878811cb4d5741a6c028d678
kadtewsd <26476963+kadtewsd@users.noreply.github.com>
2026-02-28 22:00:22 +0900

    Wifi につながっていなくても写真選択できる問題を修正 (#90)

--- omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/ui/WifiSettingsCard.kt
+++ omoide-memory-uploader/app/src/main/kotlin/com/kasakaid/omoidememory/ui/WifiSettingsCard.kt
@@ -37,99 +37,12 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
-@HiltViewModel
-class WifiSettingViewModel
-    @Inject
-    constructor(
-        wifiRepository: WifiRepository,
-        private val omoideUploadPrefsRepository: OmoideUploadPrefsRepository,
-    ) : ViewModel() {
-        // 権限状態を管理する Flow
-        private val hasPermission = MutableStateFlow(false)
-
-        // observeWifiSSID() を「実行」して、StateFlow に変換して保持する
-        // これにより、リスナーの登録は「ViewModel の生存期間中、一回だけ」になる
-        // callbackFlow は、誰かが collect（購読）を開始するたびに中身のブロックが最初から実行される**「Cold Flow」**です。
-        // stateIn なし: 画面が再描画（Recomposition）されるたびに registerNetworkCallback と unregister が繰り返され、OS側が「短時間に何度も登録しすぎ」と判断して値を返さなくなったり、挙動が不安定になります。
-        // first だと一度実行したら川が終わってしまう。flatMapLatest を使って川を流したままにする
-        @OptIn(ExperimentalCoroutinesApi::class)
-        val wifiState: StateFlow<WifiSetting> =
-            hasPermission
-                .flatMapLatest { granted ->
-                    if (granted) {
-                        // 権限がある時だけ、OSの監視を開始する
-                        wifiRepository.observeWifiSSID()
-                    } else {
-                        flowOf(WifiSetting.Idle)
-                    }
-                }.stateIn(
-                    scope = viewModelScope,
-                    // アップロード後に戻ってくるので、SharingStarted.WhileSubscribed(5000) ではまずいため。切ってしまってはいけない
-                    started = SharingStarted.Eagerly,
-                    initialValue = WifiSetting.Idle,
-                )
-
-        // 権限が変わったことを外部から伝える
-        fun updatePermissionStatus(isGranted: Boolean) {
-            hasPermission.value = isGranted
-        }
-
-        /**
-         * 確定した Wifi の SSID を取得する
-         */
-        private val _fixedSecureWifiSsid: MutableStateFlow<String?> =
-            MutableStateFlow(
-                omoideUploadPrefsRepository.getSecureWifiSsid(),
-            )
-        val fixedSecureWifiSsid = _fixedSecureWifiSsid.asStateFlow()
-
-        fun changeWifiState(ssid: String) {
-            // 再開可能な関数はないです。値をセットすることでいきなりイベントが伝播しますよ
-            _fixedSecureWifiSsid.value = ssid
-            omoideUploadPrefsRepository.saveSecureWifiSsid(ssid)
-        }
-    }
-
 @Composable
-fun WifiSettingsCardRoute(
-    viewModel: WifiSettingViewModel = hiltViewModel(),
-    isPermissionGranted: Boolean,
-    // context, viewModel などを渡すとメモリリークが出るかもしれない。呼び出し先は関数などをもらうだけにする
-    // context: Context,
-) {
-    // isPermissionGranted が変化するたびに ViewModel の中の Flow を叩き直す
-    // LaunchedEffect に Unit を渡すと Composable が最初に実行されたときだけ動く、と言う挙動になる。
-    // 一方で、引数を設定すると値が変わった時に動くことになる
-    LaunchedEffect(isPermissionGranted) {
-        viewModel.updatePermissionStatus(isPermissionGranted)
-    }
-
-    // by は Android の副作用を伝搬させるもの。値の監視は by を使うことによって行われる
-    // 値がかわったことが監視されると copy を使って新規インスタンスが再度生成される
-    // 値の変更が検知されたら。その Composable 関数だけが「新しい State インスタンス」を引数にして叩き直される。
-    val wifiSetting by viewModel.wifiState.collectAsState()
-
-    val fixedSecureWifiSsid by viewModel.fixedSecureWifiSsid.collectAsState()
-
-    WifiSettingsCard(
-        fixedSecureSsid = fixedSecureWifiSsid,
-        wifiSetting = wifiSetting,
-        onFixSecureSsid = {
-            viewModel.changeWifiState(it.ssid)
-        },
-        isPermissionGranted = isPermissionGranted,
-    )
-}
-
-@Composable
-private fun WifiSettingsCard(
+fun WifiSettingsCard(
     fixedSecureSsid: String?,
     wifiSetting: WifiSetting,
-    onFixSecureSsid: (WifiSetting.Found) -> Unit,
+    onFixSecureSsid: (String) -> Unit,
     isPermissionGranted: Boolean,
-    // context, viewModel などを渡すとメモリリークが出るかもしれない。呼び出し先は関数などをもらうだけにする
-//    context: Context,
-//    viewModel: MainViewModel,
 ) {
     if (!isPermissionGranted) {
         Text("接続許可がないため Wifi を取得できないです。")
@@ -169,7 +82,7 @@ private fun WifiSettingsCard(
                         )
                         if (wifiSetting.ssid == fixedSecureSsid) {
                             // 既存の設定を今回の Wifi に上書きする場合の選択肢
-                            Button(onClick = { onFixSecureSsid(wifiSetting) }) { Text("現在の Wifi をセットする") }
+                            Button(onClick = { onFixSecureSsid(wifiSetting.ssid) }) { Text("現在の Wifi をセットする") }
                         }
                     } else {
                         Text(
@@ -178,7 +91,7 @@ private fun WifiSettingsCard(
                             style = MaterialTheme.typography.bodyLarge,
                         )
                         Button(
-                            onClick = { onFixSecureSsid(wifiSetting) },
+                            onClick = { onFixSecureSsid(wifiSetting.ssid) },
                             modifier = Modifier.padding(top = 8.dp),
                         ) {
                             // ここに入るたびに、onFixSecureSsid をするとなにかをするたびに無駄に動く。ボタンを押した時だけにする
