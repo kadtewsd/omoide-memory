4f430e85c05ac680289ba8ab7f4fdbe3439d3e02
kadtewsd <26476963+kadtewsd@users.noreply.github.com>
2026-09-15 01:02:35 +0900

    フロントエンドで動かなかった問題を修正 (#202)

--- omoide-memory-sharing/frontend/eslint.config.js
+++ omoide-memory-sharing/frontend/eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
@@ -15,9 +14,6 @@ export default tseslint.config([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
-    plugins: {
-      react,
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -27,23 +23,14 @@ export default tseslint.config([
         },
       },
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
     rules: {
       // TypeScript関連
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
-
-      // React関連
-      'react/jsx-uses-react': 'off', // React 17+ では不要
-      'react/react-in-jsx-scope': 'off', // React 17+ では不要
-      'react/prop-types': 'off', // TypeScriptを使用しているため
 
       // その他
       'no-console': 'warn',
     },
   },
 ])
+
